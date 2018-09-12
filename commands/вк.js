daa6eb54e0237d7563b79714d1a30dbf0c77cb16
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-09-12 22:30:06 +0300

    Update вк.js

--- commands/вк.js
+++ commands/вк.js
@@ -3,8 +3,7 @@ exports.run = (client, message) => {
 
 	const embed = new Discord.RichEmbed()
 	.setAuthor("Всем ребятам привет!")
-	.setDescription("Ребята. Очень просим вас подписаться на [нашу группу](https://vk.com/izbushkadryzhba) в ВК\nМы очень хоти иметь активную группу, по этому не ленитесь)")
-	.setThumbnail(client.user.avatarURL)
+	.setDescription("Ребята. Очень просим вас подписаться на [нашу группу](https://vk.com/izbushkadryzhba) в ВК")
 	.setFooter('C уважением - Администрация проекта "Дружелюбная изба"', client.user.avatarURL)
 	.setColor("#FFCD00")
 	.setImage("https://addons-media.operacdn.com/media/CACHE/images/themes/44/226044/1.0-rev1/images/131c62c3e4c72d278e8f6116aa48b5a7/6e4454ca553040420b160a5da4577e52.jpg")
