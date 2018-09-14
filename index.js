b80fa4c1056f24ad84e5ce3382d7f3b09dde19b2
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-09-14 21:28:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const mysql = require("mysql")
 const prefix = '-';
 
 const con = mysql.createConnection({
-    host: "db4free.net",
-    user: "botdrizba",
-    password: "drizba123",
-    database: "drizba"
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 con.connect(function(err) {
     if (err) throw err;
