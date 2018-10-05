9dcc6ad6c4c7672cb0fedc528056a9c6a406327d
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-05 22:08:03 +0300

    Update роль.js

--- commands/роль.js
+++ commands/роль.js
@@ -2,10 +2,10 @@ exports.run = async (client, message, [command, mention, commandname, ...message
     const Discord = require("discord.js");
     const mysql = require("mysql");
     const con = mysql.createConnection({
-        host: "db4free.net",
-        user: "botdrizba",
-        password: "drizba123",
-        database: "drizba"
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME
     });
 
     const mentionRole = message.mentions.roles.first();
