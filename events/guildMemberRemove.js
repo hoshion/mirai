b2ea18de35590c52f24280f76ecaffcf5512f300
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-06 22:20:06 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,10 +2,10 @@ exports.run = (client, member) => {
     const Discord = require("discord.js");
     const mysql = require("mysql");
     const con = mysql.createConnection({
-        host: "db4free.net",
-        user: "botdrizba",
-        password: "drizba123",
-        database: "drizba"
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME
     });
     console.log("Покинул " + member.user.username);
     con.query(`DELETE FROM test WHERE userid = ${member.id}`);
