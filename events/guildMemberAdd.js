d7279703e44c86b3671cf92500a1259890409722
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-06 23:08:41 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -12,7 +12,7 @@ exports.run = async (client, member) => {
     });
 
 	member.send("Дружок, я вижу ты зашёл на сервер ***Kyoto City***. Будь хорошим мальчиком и старайся общаться. Советуем заглянуть в комнату " + member.guild.channels.get("497834668981354497") + ", " + member.guild.channels.get("497834766679277569") + ", " + member.guild.channels.get("497836123725496322") + "и " + member.guild.channels.get("497835838147657749") + ". Получай опыт и становись выше других.");
-    member.addRole("497821551014576131");
+    member.addRole("498218442034249728");
     const tag = member.user.tag;
     const avatar = member.user.avatarURL;
   	    Jimp.read(avatar).then(avatar => {
