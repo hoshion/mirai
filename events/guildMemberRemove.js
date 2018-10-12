24358031104d3ad02cfe05a6a423aee4bd966394
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-12 22:56:57 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -7,6 +7,8 @@ exports.run = (client, member) => {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME
     });
+    const channel = member.guild.channels.get("497837691665121290");
+    channel.send(member + " покинул... Жаль.")
     console.log("Покинул " + member.user.username);
     con.query(`DELETE FROM test WHERE userid = ${member.id}`);
 };
