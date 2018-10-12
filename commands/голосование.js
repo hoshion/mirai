7685ff3e0644bcaca7c5546c60a5207225d10355
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-12 20:56:50 +0300

    Update голосование.js

--- commands/голосование.js
+++ commands/голосование.js
@@ -1,7 +1,11 @@
 exports.run = (client, message) => {
 	const args = message.content.slice(14).trim().split(` "`);
 	function argsReturn(number){
-	if(!number) return " " else return number;
+		if(!number) {
+			return " "
+		} else {
+			return number;
+		}
 	}
 	message.channel.send(args[0] + args[1] + argsReturn(args[2]));
 }
