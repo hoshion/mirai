a77feed7e973f92dd1fce5a2ea718c4644b3c2fd
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-20 18:20:08 +0300

    Update инфо.js

--- commands/инфо.js
+++ commands/инфо.js
@@ -2,6 +2,7 @@ exports.run = (client, message) => {
     const anotherMember = message.mentions.members.first();
     if(!anotherMember){
         message.channel.send("Аккаунт создан : " + message.member.user.createdAt);
+        return
     }
     message.channel.send("Аккаунт создан : " + anotherMember.user.createdAt);
     console.log('Команда "инфо" использована пользователем ' + message.author.username + '. Результат - успешно');
