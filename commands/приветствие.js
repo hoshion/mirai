6b884693f80cb4c4e8e6f02be28ba7340d4bfa88
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-20 16:41:28 +0300

    Update приветствие.js

--- commands/приветствие.js
+++ commands/приветствие.js
@@ -11,7 +11,20 @@ exports.run = (client, message, args) => {
 	con.query(`SELECT * FROM local WHERE serverid = '${message.guild.id}' AND type = 'welcome'`, function(err, result){
 		if(!args[0]){
 			if(!result[0]) return message.channel.send("**У вас не установлено приветствие!**\n\nЧтобы его установить напишите `-приветствие установить [текст приветствия]`")
-			message.channel.send(``)
+			message.channel.send(`Приветствие на этом сервере ー　**${result[0]}.message}**`)
+		}
+		if(args[0].toLowerCase() == "добавить"){
+			if(!result[0]){
+				con.query(`INSERT INTO local (serverid,type,message) VALUES('${message.guild.id}','welcome','${args.join(" ")}')`, function(err){
+					if(err) console.log(err);
+					message.channel.send(`Вы установили приветствие!`)
+				})
+			} else {
+				con.query(`UPDATE local SET message = ${args.join(" ")} WHERE serverid = '${message.guild.id}' AND type = 'welcome'`, function(err){
+					if(err) console.log(err);
+					message.channel.send(`Вы установили приветствие!`)
+				})
+			}
 		}
 	})
 }
