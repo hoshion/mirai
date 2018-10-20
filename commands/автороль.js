1665635ffeeab8a9974a76295b3b05316be70b6a
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-20 15:00:40 +0300

    Update автороль.js

--- commands/автороль.js
+++ commands/автороль.js
@@ -9,16 +9,18 @@ exports.run = (client, message, args) => {
 	con.query(`SELECT * FROM local WHERE serverid = '${message.guild.id}' AND type = 'autorole'`,function (err,result){
 		if(!args[0]){
 			if(!result[0]) return message.channel.send("**У вас нету установленной автороли!**\n\nЧтобы установить автороль, напишите `-автороль добавить @роль`")
-			message.channel,send(`Установленная автороль на сервере ー **${message.guild.roles.get(result[0].roleid).name}**`)
+			message.channel.send(`Установленная автороль на сервере ー **${message.guild.roles.get(result[0].roleid).name}**`)
 		}
 		if(args[0].toLowerCase() == "добавить"){
 			if(!result[0]){
 				con.query(`INSERT INTO local (serverid,type,roleid) VALUES('${message.guild.id}','autorole','${message.mentions.roles.first().id}')`, function(err){
 					if(err) console.log(err);
+					message.channel.send(`Вы установили роль **${message.guild.roles.get(result[0].roleid).name}** по умолчанию`)
 				})
 			} else {
 				con.query(`UPDATE local SET roleid = ${message.mentions.roles.first().id} WHERE serverid = '${message.guild.id}' AND type = 'autorole'`, function(err){
 					if(err) console.log(err);
+					message.channel.send(`Вы установили роль **${message.guild.roles.get(result[0].roleid).name}** по умолчанию`)
 				})
 			}
 		}
