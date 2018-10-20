bcc0a06f7536f090c7e2b960a25542baa8d65f2c
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-20 16:53:43 +0300

    Update приветствие.js

--- commands/приветствие.js
+++ commands/приветствие.js
@@ -10,20 +10,35 @@ exports.run = (client, message, args) => {
 	
 	con.query(`SELECT * FROM local WHERE serverid = '${message.guild.id}' AND type = 'welcome'`, function(err, result){
 		if(!args[0]){
-			if(!result[0]) return message.channel.send("**У вас не установлено приветствие!**\n\nЧтобы его установить напишите `-приветствие установить [текст приветствия]`")
+			if(!result[0]) return message.channel.send("**У вас не установлено приветствие!**\n\nЧтобы его установить напишите `-приветствие добавить [текст приветствия]\nЧтобы установить канал для приветствия, напишите `-приветствие добавить #канал`")
 			message.channel.send(`Приветствие на этом сервере ー　**${result[0]}.message}**`)
+			return;
 		}
 		if(args[0].toLowerCase() == "добавить"){
-			if(!result[0]){
-				con.query(`INSERT INTO local (serverid,type,message) VALUES('${message.guild.id}','welcome','${args.join(" ")}')`, function(err){
-					if(err) console.log(err);
-					message.channel.send(`Вы установили приветствие!`)
-				})
+			if(!message.mentions.channels.first){
+				if(!result[0]){
+					con.query(`INSERT INTO local (serverid,type,message) VALUES('${message.guild.id}','welcome','${args.join(" ")}')`, function(err){
+						if(err) console.log(err);
+						message.channel.send(`Вы установили приветствие!`)
+					})
+				} else {
+					con.query(`UPDATE local SET message = ${args.join(" ")} WHERE serverid = '${message.guild.id}' AND type = 'welcome'`, function(err){
+						if(err) console.log(err);
+						message.channel.send(`Вы установили приветствие!`)
+					})
+				}
 			} else {
-				con.query(`UPDATE local SET message = ${args.join(" ")} WHERE serverid = '${message.guild.id}' AND type = 'welcome'`, function(err){
-					if(err) console.log(err);
-					message.channel.send(`Вы установили приветствие!`)
-				})
+				if(!result[0]){
+					con.query(`INSERT INTO local (serverid,type,channelid) VALUES('${message.guild.id}','welcome','${message.mentions.channels.first().id}')`, function(err){
+						if(err) console.log(err);
+						message.channel.send(`Вы установили канал ${message.mentions.channels.first().id} для приветствия!`)
+					})
+				} else {
+					con.query(`UPDATE local SET channelid = ${message.mentions.channels.first().id} WHERE serverid = '${message.guild.id}' AND type = 'welcome'`, function(err){
+						if(err) console.log(err);
+						message.channel.send(`Вы установили канал ${message.mentions.channels.first().id} для приветствия!`)
+					})
+				}
 			}
 		}
 	})
