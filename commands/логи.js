24e78e0a7f8a4211052858cff038cf4f2a615459
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-20 13:39:24 +0300

    Update логи.js

--- commands/логи.js
+++ commands/логи.js
@@ -8,8 +8,9 @@ exports.run = (client, message, args) => {
     });
     
     if(args[0].toLowerCase() == "установить"){
-        con.query(`INSERT INTO local (serverid, type, channelid) VALUES('${message.guild.id}','logs', '${message.mentions.channels.first().id}')`)
+        con.query(`INSERT INTO local (serverid, type, channelid) VALUES('${message.guild.id}','logs', '${message.mentions.channels.first().id}')`, function(err){
+        if(err) console.log(err);
+        })
         message.channel.send(`Успешно установлен канал **${message.mentions.channels.first().id}** для логов!`)
-        )
     }
 }
