3f734a78903d700ff4bc0e55eaf9c58194d14a50
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-20 19:46:19 +0300

    Update расчёт.js

--- commands/расчёт.js
+++ commands/расчёт.js
@@ -1,9 +1,10 @@
 exports.run = (client, message) => {
 	for(let i = 100; i < 156000;){
-		for(let n = 0; n = n + 1; n++){
 		i = i + i * 0.1
+		let n;
+		n = 1
+		n = n + 1;
 		console.log(i)
 		console.log(n)
-		}
 	}
 }
