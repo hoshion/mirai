55ceb0eb2cd7f6f56731bb9df99c640cde138383
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-20 19:23:28 +0300

    Update расчёт.js

--- commands/расчёт.js
+++ commands/расчёт.js
@@ -1,8 +1,6 @@
 exports.run = (client, message) => {
 	let st;
-	for(let st = 100; st < 156000){
-		st = 100;
-		st = st + (st * 0.3)
+	for(let st = 100; st < 156000; st = st + st * 0,3){
 		message.channel.send(st);
 	}
 }
