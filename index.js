b5d9e336ca77d1d27794c6fa60a0b08326ed826d
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-22 14:06:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -83,9 +83,11 @@ client.on('message', function(message){
 			} else {
 				youtube.searchVideos(searchString, 1)
 				.then(videos => {
-					const video = youtube.getVideoByID(videos[0].id);
+					const video = youtube.getVideoByID(videos.id);
 					console.log("Дальше videos[0]")
 					console.log(videos[0]);
+					console.log("videos[0].id");
+					console.log(videos[0].id);
 					console.log("Дальше video")
 					console.log(video)
 					return videoHandler(video, message, voiceChannel);
