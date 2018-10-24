a118dc1310050a1bd1ef8aa7221cd776d0d855d6
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-24 10:27:54 +0300

    Create placement.js

---
+++ placement.js
@@ -0,0 +1,15 @@
+exports.run = (client, message, args) => {
+	const mysql = require("mysql")
+	const con = mysql.createConnection({
+		host: process.env.DATABASE_HOST,
+		user: process.env.DATABASE_USER,
+		password: process.env.DATABASE_PASSWORD,
+		database: process.env.DATABASE_NAME
+	});
+	
+	con.query(`SELECT * FROM local WHERE serverid = ${message.guild.id} AND type = member ORDER BY local.xp DESC`, function(err, result){
+		con.query(`SELECT * FROM local WHERE serverid = ${message.guild.id} AND userid = ${message.author.id}`, function(err, result2){
+			message.channel.send(result.xp.join(" ")); 
+		});
+	});
+};
