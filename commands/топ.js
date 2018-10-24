eaa15a114437c81c4d9630426b2b98730b959580
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-24 17:54:56 +0300

    Update топ.js

--- commands/топ.js
+++ commands/топ.js
@@ -6,7 +6,7 @@ exports.run = (client, message) => {
         password: "drizba123",
         database: "drizba"
     });
-    let number = con.query(`SELECT * FROM test ORDER by test.xp DESC`, function(err, result){
+    let number = con.query(`SELECT * FROM local ORDER by local.xp DESC`, function(err, result){
         if(err) throw err;
         if(message.author.id == result[0].userid){
             number = 1;
@@ -44,7 +44,7 @@ exports.run = (client, message) => {
         XP = result[0].xp;
     })
 
-    con.query(`SELECT userid, xp FROM test ORDER by test.xp DESC`, function(err, top){
+    con.query(`SELECT userid, xp FROM local ORDER by test.xp DESC`, function(err, top){
         if(err) throw err;
         message.channel.send("**```ini\n[1 место] - " + message.guild.members.get(top[0].userid).user.username + "\n    XP ; " + top[0].xp + 
         "\n[2 место] - " + message.guild.members.get(top[1].userid).user.username + "\n    XP ; " + top[1].xp + 
