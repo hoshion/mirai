f2461a42b8d9ff67ec75bf26d0a86a26b4390b55
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-25 19:22:38 +0300

    Update аватар.js

--- commands/аватар.js
+++ commands/аватар.js
@@ -1,12 +1,11 @@
 exports.run = (client, message) => {
     const Discord = require('discord.js');
     const otherAvatar = message.mentions.members.first();
-    const embedAvatar = new Discord.RichEmbed().setImage(message.author.avatarURL).setColor("ffda8b");
+    const embedAvatar = new Discord.RichEmbed().setAuthor(`URL`, null, message.author.avatarURL).setImage(message.author.avatarURL).setColor("ffda8b");
     if(!otherAvatar){
         message.channel.send(embedAvatar).catch((err) => console.log('Команда "аватар" использована пользователем' + message.author.username + '. Результат - ошибка :'));
     } else {
     const embedAvatarOther = new Discord.RichEmbed().setImage(otherAvatar.user.avatarURL).setColor("ffda8b");
     message.channel.send(embedAvatarOther).catch((err) => console.log('Команда "аватар" использована пользователем' + message.author.username + '. Результат - ошибка :'));
     }
-    console.log('Команда "аватар" использована пользователем ' + message.author.username + '. Результат - успешно');
 }
