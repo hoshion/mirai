f7cdc5473c0243b60fb5cac5d7bb111915b10929
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-25 20:07:37 +0300

    Update бан.js

--- commands/бан.js
+++ commands/бан.js
@@ -1,7 +1,14 @@
 exports.run = (client, message, [mention, ...reason]) => {
     const Discord = require("discord.js");
     var member = message.mentions.members.first();
-
+    const mysql = require("mysql")
+    
+    const con = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME
+    });
     if(!member) return message.channel.send("Вы не указали человека!");
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Недостаточно прав!");
 
@@ -10,11 +17,15 @@ exports.run = (client, message, [mention, ...reason]) => {
     member.ban(reason.join(" "))
     .then(() => message.channel.send("Вы забанили **" + member.user.tag + "** по причине - **" + reason + "** !"))
     .catch(() => message.channel.send("Чота у него дофига прав."));
-    const channel = message.guild.channels.get("497837552636788737");
-    const embed = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription("Забанил человека " + member.user.tag + "\nПо причине : " + reason.join(" "))
-    .setColor("ff0000")
-    .setFooter("Ваш бот - Верховный суд", client.user.avatarURL);
-    channel.send(embed);
+	con.query(`SELECT * FROM local WHERE serverid = ${message.guild.id} AND type = 'logs'`, function(err, result){
+		if(err) console.log(err);
+		if(!result[0]) return;
+		const channel = message.guild.channels.get("497837552636788737");
+		const embed = new Discord.RichEmbed()
+		.setAuthor(message.author.username, message.author.avatarURL)
+		.setDescription(`Кикнул человека ${member.user.username}\nПо причине : ${reason.join(" ")}`)
+		.setColor(`ff0000`)
+		.setFooter(`Ваш бот - ${client.user.username}`, client.user.avatarURL);
+		channel.send(embed);
+	})
 }
