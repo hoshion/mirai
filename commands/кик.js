aa5b32b016b41f6e857d4fb7bf35609e3138725f
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-25 19:48:22 +0300

    Update кик.js

--- commands/кик.js
+++ commands/кик.js
@@ -1,20 +1,30 @@
 exports.run = (client, message, [mention, ...reason]) => {
     const Discord = require("discord.js");
     var member = message.mentions.members.first();
+    const mysql = require("mysql")
+	
+    const con = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME
+    });
 
     if(!member) return message.channel.send("Вы не указали человека!");
     if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Недостаточно прав!");
 
-    console.log(message.author.username + " Использовал команду -кик @" + member.user.username);
-
-    member.kick(reason.join(""))
+    member.kick(reason.join(" "))
     .then(() => message.channel.send("Вы кикнули **" + member.user.username + "** по причине **" + reason + "** !"))
     .catch(() => message.channel.send("Чота у него дофига прав."));
-    const channel = message.guild.channels.get("497837552636788737");
-    const embed = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription("Кикнул человека " + member.user.username + "\nПо причине : " + reason.join(" "))
-    .setColor("ff0000")
-    .setFooter("Ваш бот - Дружелюбная изба", client.user.avatarURL);
-    channel.send(embed);
+	con.query(`SELECT * FROM local WHERE serverid = ${message.guild.id} AND type = 'logs'`, function(err, result){
+		if(err) console.log(err);
+		if(!result[0]) return;
+		const channel = message.guild.channels.get("497837552636788737");
+		const embed = new Discord.RichEmbed()
+		.setAuthor(message.author.username, message.author.avatarURL)
+		.setDescription(`Кикнул человека ${member.user.username}\nПо причине : ${reason.join(" ")}`)
+		.setColor(`ff0000`)
+		.setFooter(`Ваш бот - ${client.user.username}`, client.user.avatarURL);
+		channel.send(embed);
+	})
 }
