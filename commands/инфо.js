d96c123527560eb314a9e0e900f66d8457a0ffce
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-10-26 11:23:06 +0300

    Update инфо.js

--- commands/инфо.js
+++ commands/инфо.js
@@ -1,9 +1,22 @@
 exports.run = (client, message) => {
+    const Discord = require('discord.js');
     const anotherMember = message.mentions.members.first();
     if(!anotherMember){
-        message.channel.send("Аккаунт создан : " + message.member.user.createdAt);
-        return
-    }
-    message.channel.send("Аккаунт создан : " + anotherMember.user.createdAt);
-    console.log('Команда "инфо" использована пользователем ' + message.author.username + '. Результат - успешно');
+		const embed = Discord.RichEmbed()
+			.setAuthor(message.author.username)
+			.setThumbnail(message.author.avatarURL)
+			.addField(`ID`, message.author.id, true)
+			.addField(`Никнейм`, message.member.nickname, true)
+			.addField(`Аккаунт создан`, message.author.createdAt)
+			.addField(`Вступил на сервер`, message.member.joinedAt)
+		return message.channel.send(embed);
+	}
+	const embed = Discord.RichEmbed()
+		.setAuthor(anotherMember.user.username)
+		.setThumbnail(anotherMember.user.avatarURL)
+		.addField(`ID`, anotherMember.user.id, true)
+		.addField(`Никнейм`, anotherMember.nickname, true)
+		.addField(`Аккаунт создан`, anotherMember.user.createdAt)
+		.addField(`Вступил на сервер`, anotherMember.joinedAt)
+	return message.channel.send(embed);
 }
