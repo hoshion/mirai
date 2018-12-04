99f96f34cd8dbc22ff3f12c7c2eab92a81f73590
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-05 00:29:02 +0200

    Update скажи.js

--- commands/скажи.js
+++ commands/скажи.js
@@ -2,6 +2,6 @@ exports.run = async (client, message, args) => {
     await message.channel.send(args.join(" "));
     await message.delete();
 }
-exports.help = (client, message, args) => {
+exports.help = (client, message) => {
     message.channel.send(`**Информация о команде "скажи"**\n\nС помощью этой команды вы можете писать со стороны бота\nЧтобы воспользоваться этой командой, напишите \`-скажи текст\``);
 }
