b5da165a7c30f083d735ca2d69ebadbdcc26e35e
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-05 00:02:30 +0200

    Update скажи.js

--- commands/скажи.js
+++ commands/скажи.js
@@ -2,3 +2,6 @@ exports.run = async (client, message, args) => {
     await message.channel.send(args.join(" "));
     await message.delete();
 }
+exports.help = (client, message, args) => {
+    message.channel.send(`Проверка`);
+}
