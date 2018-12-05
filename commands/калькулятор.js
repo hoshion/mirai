8c6f8f389f2e133c7bf4aa3d5a8ab7a06d8fbf45
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-05 19:18:06 +0200

    Update калькулятор.js

--- commands/калькулятор.js
+++ commands/калькулятор.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     const number2 = args[2];
     if(!args[3]){
         if(sign1 == "+"){
-            const calc = parseInt(number1) + parseInt(number2);
+            const calc = eval(number1 sign1 number2);
             message.channel.send(calc);
         }
         if(sign1 == "-"){
@@ -16,7 +16,7 @@ exports.run = (client, message, args) => {
             message.channel.send(calc);
         }
         if(sign1 == ":"){
-            if(number2 == 0) return message.channel.send("НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ СУКА!");
+            if(number2 == 0) return message.channel.send("На ноль делить нельзя!");
             const calc = parseInt(number1) / parseInt(number2);
             message.channel.send(calc);
         }
@@ -25,4 +25,7 @@ exports.run = (client, message, args) => {
             message.channel.send(calc);
         }
     }
+}
+exports.help = (client, message) => {
+    message.channel.send(`**Информация о команде "калькулятор"**\n\nС её помощью, вы сможете делать простейшие действия с 2 числами.\nЧтобы использовать, напишите \`-калькулятор число знак число\``)
 }
