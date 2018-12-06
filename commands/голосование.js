8781710b3b1e3566a296b29778812bf2d9ca65da
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-06 16:47:56 +0200

    Update голосование.js

--- commands/голосование.js
+++ commands/голосование.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
 	let b;
 	for(let i = 0; i < args.size; i++){
-		let a = args[i].match(/(.+?)/);
+		let a = args[i].match(/"(.+?)"/);
 		b.push(a[1]);
 	}
 	message.channel.send(`${b[0]} ${b[1]} ${b[2]}`);
