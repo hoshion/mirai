135359a4690c8c5e1a23cc25aa6ced714548c92b
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-06 16:32:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -153,11 +153,11 @@ client.on('message', async function(message){
     } catch (error) {
 	console.log(`Команда "${command}" была использована пользователем ${message.author.username}. Результат - ` + error);
 	message.author.send(error);
-	await fs.appendFile(`lasterror.txt`, `${error}`, function(err){
+	/*await fs.appendFile(`lasterror.txt`, `${error}`, function(err){
 		if(err) console.log(err)
 		console.log("Saved!")
 	});
-	message.author.send({files: [{name: `lasterror.txt`}]});
+	await message.author.send(`Ура`,{files: [{name: `lasterror.txt`}]});*/
 	con.query(`SELECT * FROM local WHERE serverid = '${message.guild.id}' AND command = '${command}'`, function(err, result){
             if(!result[0]){
 		if(err) console.log(err);
