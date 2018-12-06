dec55f07f39cd09812b6edb909c826b8a692b82d
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-06 19:02:15 +0200

    Update голосование.js

--- commands/голосование.js
+++ commands/голосование.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
-	let b, c = 0;
-	console.log(c);
+	let c = 0;
+	var b = [];
 	for(var i = 0; i < args.length; i++){
 		await console.log(i)
 		let a = await args[i].match(/"(.+?)"/);
