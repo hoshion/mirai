71535a390479b0d0b4bae3de68247892138cf972
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-07 22:17:27 +0200

    Update кубик.js

--- commands/кубик.js
+++ commands/кубик.js
@@ -4,3 +4,6 @@ exports.run = (client, message, args) => {
     message.channel.send("Ваше число : **" + random + "**!");
     console.log('Команда "кубик" использована пользователем ' + message.author.username + '. Результат - успешно');
 }
+exports.help = (client, message) => {
+    message.channel.send(`**Информация о команде "кубик"**\n\nПозволяет выдать вам рандомное число из указанного диапазона.\nЧтобы использовать, напишите \`-кубик число\``)
+}
