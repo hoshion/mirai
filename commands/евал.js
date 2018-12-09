c024c5bacc705583f76644ccb5750f516e2df950
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-09 20:03:15 +0200

    Update евал.js

--- commands/евал.js
+++ commands/евал.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
 	if(message.author.id != `412338841651904516`) return;
 	const code   = args.join(` `);
-	let evaled = eval()
+	let evaled = eval(code)
     if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
 	message.channel.send(evaled, {code:"xl"});
 }
