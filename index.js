4017a1d20d5e1ff6a9a66f863c8b5bf263bc8d2c
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-10 19:45:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ client.on('message', function(message){
 								if(!message.guild.members.get(result[0].userid).roles.has(result2[i].roleid)){
 									message.member.addRole(result2[i].roleid);
 									if(i != (result2.length - 1)){
-										console.log(result2.length)
 										if(message.guild.members.get(result[0].userid).roles.has(result2[i+1].roleid)){
 											message.member.removeRole(result2[i+1].roleid);
 										}
