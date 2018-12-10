441a375285ada1822d2ca05127d7a188b7359824
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-10 10:28:20 +0200

    Update евал.js

--- commands/евал.js
+++ commands/евал.js
@@ -1,9 +1,18 @@
-exports.run = (client, message, args) => {
-	if(message.author.id != `412338841651904516`) return;
-	const code   = args.join(` `);
-	let evaled = eval(code)
-    if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-	message.channel.send(clean(evaled));
+exports.run = (client, message) => {
+	if(message.author.id !== `412338841651904516`) return;
+	const args = message.content.split(" ").slice(1);
+	
+	try {
+		const code = args.join(` `);
+		let evaled = eval(code)
+		
+		if (typeof evaled !== "string")
+			evaled = require("util").inspect(evaled);
+		
+		message.channel.send(clean(evaled), {code:`xl`});
+	} catch(err) {
+		client.fetchUser('412338841651904516').then(user => user.send(`\`\`\`javascript\n${err.stack}\`\`\``))
+	}
 }
 function clean(text) {
   if (typeof(text) === "string")
