75e39badb275d31f16d14db7361e4b7ed872e626
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-10 10:22:01 +0200

    Update евал.js

--- commands/евал.js
+++ commands/евал.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
 	message.channel.send(clean(evaled));
 }
-const clean = text => {
+function clean(text) {
   if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
   else
