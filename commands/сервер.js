baae81b0c1e2cc6f8ce803b30471905198dd10e4
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-15 16:37:51 +0200

    Update сервер.js

--- commands/сервер.js
+++ commands/сервер.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
        .addField('Владелец', message.guild.owner, true)
        .addField('id владельца', message.guild.ownerID, true)
        .addField('Роли', message.guild.roles.size, true)
-       .addField('Регион', message.guild.region, true)
+       .addField('Регион', regionRename(message.guild.region), true)
        .addField('Участники', `${message.guild.presences.size} в сети\n${message.guild.memberCount} всего`, true)
        .addField('AFK канал', message.guild.afkChannel, true)
        .addField('Каналы', `${message.guild.channels.filter(c => c.type == 'voice').size} текстовых\n${message.guild.channels.filter(c => c.type == 'voice').size} голосовых`, true)
