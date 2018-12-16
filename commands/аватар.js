92dcdce6f3df92b1ae038f141be849e0ea9d8104
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-16 15:07:13 +0200

    Update аватар.js

--- commands/аватар.js
+++ commands/аватар.js
@@ -1,12 +1,16 @@
 exports.run = (client, message) => {
     const Discord = require('discord.js');
-    const otherAvatar = message.mentions.members.first();
-    const embedAvatar = new Discord.RichEmbed().setDescription(`[Ссылка на аватар](${message.author.avatarURL})`).setImage(message.author.avatarURL).setColor("ffda8b");
-    if(!otherAvatar){
-        message.channel.send(embedAvatar).catch((err) => console.log('Команда "аватар" использована пользователем' + message.author.username + '. Результат - ошибка :'));
-    } else {
-    const embedAvatarOther = new Discord.RichEmbed().setImage(otherAvatar.user.avatarURL).setColor("ffda8b");
-    message.channel.send(embedAvatarOther).catch((err) => console.log('Команда "аватар" использована пользователем' + message.author.username + '. Результат - ошибка :'));
+    try {
+        const otherAvatar = message.mentions.members.first();
+        const embedAvatar = new Discord.RichEmbed().setDescription(`[Ссылка на аватар](${message.author.avatarURL})`).setImage(message.author.avatarURL).setColor("ffda8b");
+        if(!otherAvatar){
+            message.channel.send(embedAvatar).catch((err) => console.log('Команда "аватар" использована пользователем' + message.author.username + '. Результат - ошибка :'));
+        } else {
+            const embedAvatarOther = new Discord.RichEmbed().setImage(otherAvatar.user.avatarURL).setColor("ffda8b");
+            message.channel.send(embedAvatarOther).catch((err) => console.log('Команда "аватар" использована пользователем' + message.author.username + '. Результат - ошибка :'));
+        }
+    } catch(err) {
+        client.fetchUser('412338841651904516').then(user => user.send(`\`\`\`javascript\n${err.stack}\`\`\``));
     }
 }
 exports.help = (client, message) => {
