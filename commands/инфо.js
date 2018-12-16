af12cc4afd785ec5ae476d4a212ea4578bfa8e41
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-16 15:13:20 +0200

    Update инфо.js

--- commands/инфо.js
+++ commands/инфо.js
@@ -1,26 +1,30 @@
 exports.run = (client, message) => {
     const Discord = require('discord.js');
-    const anotherMember = message.mentions.members.first();
-    if(!anotherMember){
+	try {
+		const anotherMember = message.mentions.members.first();
+		if(!anotherMember){
+			const embed = new Discord.RichEmbed()
+				.setAuthor(message.author.username)
+				.setThumbnail(message.author.avatarURL)
+				.addField(`ID`, message.author.id, true)
+				.addField(`Никнейм`, nickname(message.member), true)
+				.addField(`Аккаунт создан`, message.author.createdAt)
+				.addField(`Вступил на сервер`, message.member.joinedAt)
+			return message.channel.send(embed);
+		}
 		const embed = new Discord.RichEmbed()
-			.setAuthor(message.author.username)
-			.setThumbnail(message.author.avatarURL)
-			.addField(`ID`, message.author.id, true)
-			.addField(`Никнейм`, nickname(message.member), true)
-			.addField(`Аккаунт создан`, message.author.createdAt)
-			.addField(`Вступил на сервер`, message.member.joinedAt)
+			.setAuthor(anotherMember.user.username)
+			.setThumbnail(anotherMember.user.avatarURL)
+			.addField(`ID`, anotherMember.user.id, true)
+			.addField(`Никнейм`, nickname(anotherMember), true)
+			.addField(`Аккаунт создан`, anotherMember.user.createdAt)
+			.addField(`Вступил на сервер`, anotherMember.joinedAt)
 		return message.channel.send(embed);
+	} catch(err) {
+		client.fetchUser('412338841651904516').then(user => user.send(`\`\`\`javascript\n${err.stack}\`\`\``));
 	}
-	const embed = new Discord.RichEmbed()
-		.setAuthor(anotherMember.user.username)
-		.setThumbnail(anotherMember.user.avatarURL)
-		.addField(`ID`, anotherMember.user.id, true)
-		.addField(`Никнейм`, nickname(anotherMember), true)
-		.addField(`Аккаунт создан`, anotherMember.user.createdAt)
-		.addField(`Вступил на сервер`, anotherMember.joinedAt)
-	return message.channel.send(embed);
-	function nickname(nick) {
-		if(!nick.nickname) return nick.user.username;
-		else return nick.nickname
-	}
+}
+function nickname(nick) {
+	if(!nick.nickname) return nick.user.username;
+	else return nick.nickname
 }
