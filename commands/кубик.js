278eefa9fb0baa6e50131f88c0dbcdd785f088c2
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-16 15:19:26 +0200

    Update кубик.js

--- commands/кубик.js
+++ commands/кубик.js
@@ -1,8 +1,11 @@
 exports.run = (client, message, args) => {
-    if(!args[0]) return message.channel.send("Вы не ввели число!");
-    const random = Math.floor(Math.random() * parseInt(args[0])) + 1;
-    message.channel.send("Ваше число : **" + random + "**!");
-    console.log('Команда "кубик" использована пользователем ' + message.author.username + '. Результат - успешно');
+    try { 
+        if(!args[0]) return message.channel.send("Вы не ввели число!");
+        const random = Math.floor(Math.random() * parseInt(args[0])) + 1;
+        message.channel.send("Ваше число : **" + random + "**!");
+    } catch(err){
+        client.fetchUser('412338841651904516').then(user => user.send(`\`\`\`javascript\n${err.stack}\`\`\``));
+    }
 }
 exports.help = (client, message) => {
     message.channel.send(`**Информация о команде "кубик"**\n\nПозволяет выдать вам рандомное число из указанного диапазона.\nЧтобы использовать, напишите \`-кубик число\``)
