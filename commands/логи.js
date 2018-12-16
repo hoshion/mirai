ccecf4495a6e648650625e25d23270ae25ffc257
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2018-12-16 15:20:03 +0200

    Update логи.js

--- commands/логи.js
+++ commands/логи.js
@@ -6,11 +6,14 @@ exports.run = (client, message, args) => {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME
     });
-    
-    if(args[0].toLowerCase() == "установить"){
-        con.query(`INSERT INTO local (serverid, type, channelid) VALUES('${message.guild.id}','logs', '${message.mentions.channels.first().id}')`, function(err){
-        if(err) console.log(err);
-        })
-        message.channel.send(`Успешно установлен канал **${message.mentions.channels.first().id}** для логов!`)
+    try {
+        if(args[0].toLowerCase() == "установить"){
+            con.query(`INSERT INTO local (serverid, type, channelid) VALUES('${message.guild.id}','logs', '${message.mentions.channels.first().id}')`, function(err){
+            if(err) console.log(err);
+            })
+            message.channel.send(`Успешно установлен канал **${message.mentions.channels.first().id}** для логов!`)
+        }
+    } catch(err) {
+        client.fetchUser('412338841651904516').then(user => user.send(`\`\`\`javascript\n${err.stack}\`\`\``));
     }
 }
