3835b9855c5e76b809f0adc0126e109675ee4e4f
MWadmanYT <42845566+MWadmanYT@users.noreply.github.com>
2019-07-28 21:56:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const prefix = '-';
 const con = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD
+    password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_NAME
 });
 con.connect(function(err) {
     if (err) process.env.FEEDBACKEFFOR;
