8d552c47db5f7221386f9c0bcb848d5550f62f85
MWadmanYT <mwadman2018@gmail.com>
2019-11-05 00:31:08 +0200

    4

--- main/queries.js
+++ main/queries.js
@@ -3,7 +3,8 @@ const con = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME
+  database: process.env.DATABASE_NAME,
+  port: 3306
 });
 
 function SELECT(item, place, addition){
