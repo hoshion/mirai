885feb975157894794190ae37df435a26e7c5fd7
MWadmanYT <mwadman2018@gmail.com>
2019-11-05 18:53:56 +0200

    6

--- commands/евал.js
+++ commands/евал.js
@@ -6,7 +6,6 @@ exports.run = (client, message) => {
 	const con = mysql.createConnection({
 	    host: process.env.DATABASE_HOST,
 	    user: process.env.DATABASE_USER,
-	    password: process.env.DATABASE_PASSWORD,
 	    database: process.env.DATABASE_NAME,
 			port: 3306
 	});
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const mysqlQuery = require("./main/queries.js")
 const con = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   port: 3306
 });
