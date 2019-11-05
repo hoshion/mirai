69a490b4ec961f0404c947a80708658fd3379b57
MWadmanYT <mwadman2018@gmail.com>
2019-11-05 19:06:21 +0200

    7

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const mysqlQuery = require("./main/queries.js")
 const con = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
-  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
   port: 3306
 });
 
