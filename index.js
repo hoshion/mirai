017f0cce5c1d79c949a0e5870bfe5d701a18cb60
MWadmanYT <mwadman2018@gmail.com>
2019-11-09 23:45:16 +0200

    15

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const mysqlQuery = require("./main/queries.js")
 const con = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
 });
 
 con.connect(function(err) {
--- main/queries.js
+++ main/queries.js
@@ -2,7 +2,8 @@ const mysql = require("mysql");
 const con = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
 });
 
 function SELECT(item, place, addition){
